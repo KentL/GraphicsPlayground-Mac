a0b426c061d7c4111b1af14724969467a11ffcc5
kli <zhuoli@upei.ca>
2018-04-15 11:24:53 -0700

    Revert "untrack folders"
    This reverts commit cb95c9d071b110f2e25cf86ea581293ace37248f.

---
+++ cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/DependInfo.cmake
@@ -0,0 +1,117 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/SimpleOpenGLImageLibrary/src/SOIL.c" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/SimpleOpenGLImageLibrary/src/SOIL.c.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/SimpleOpenGLImageLibrary/src/image_DXT.c" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/SimpleOpenGLImageLibrary/src/image_DXT.c.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/SimpleOpenGLImageLibrary/src/image_helper.c" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/SimpleOpenGLImageLibrary/src/image_helper.c.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/SimpleOpenGLImageLibrary/src/stb_image_aug.c" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/SimpleOpenGLImageLibrary/src/stb_image_aug.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../GraphicsPlayground-Mac"
+  "../GraphicsPlayground-Mac/CityModelExample"
+  "../GraphicsPlayground-Mac/Interpolation"
+  "../GraphicsPlayground-Mac/RotateCube"
+  "../GraphicsPlayground-Mac/RotateTextureCube"
+  "../GraphicsPlayground-Mac/BlendExample"
+  "../GraphicsPlayground-Mac/WaterRendering"
+  "../GraphicsPlayground-Mac/ParticleSystem"
+  "../GraphicsPlayground-Mac/common"
+  "../SimpleOpenGLImageLibrary/src"
+  "../Tiny"
+  "../wolf"
+  "../."
+  "/usr/local/include"
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/Game.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/Game.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/BlendExample/BlendExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/BlendExample/BlendExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CausticExample/CausticExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CausticExample/CausticExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CausticExample/CausticPanel.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CausticExample/CausticPanel.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/AABBCollider.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/AABBCollider.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Camera.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Camera.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/CityModelExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/CityModelExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/CityModeller.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/CityModeller.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Collider.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Collider.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/CollisionDetector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/CollisionDetector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/CubemapTexture.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/CubemapTexture.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/DirectionalLightcpp.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/DirectionalLightcpp.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/FormatTranslator.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/FormatTranslator.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Frustum.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Frustum.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Node.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Node.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Ray.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Ray.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/SkyBox.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/SkyBox.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/Sphere.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/Sphere.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/SphereCollider.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/SphereCollider.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/CityModelExample/collidetest.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/CityModelExample/collidetest.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ExampleBase.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ExampleBase.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/Interpolation/InterpolationExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/Interpolation/InterpolationExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/AddVelAffector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/AddVelAffector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/ColorAffector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/ColorAffector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/ColorBlendingAffector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/ColorBlendingAffector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/Effect.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/Effect.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/Emitter.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/Emitter.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/FadeAffector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/FadeAffector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/HelperFunction.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/HelperFunction.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/ParticleSystemExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/ParticleSystemExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/ParticleSystem/ScaleAffector.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/ParticleSystem/ScaleAffector.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/Quad.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/Quad.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/RotateCube/RotateCubeExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/RotateCube/RotateCubeExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/RotateTextureCube/RotateTextureCubeExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/RotateTextureCube/RotateTextureCubeExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/WaterQuad.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/WaterQuad.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/WaterRendering/WaterRenderingExample.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/WaterRendering/WaterRenderingExample.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/common/RenderTarget.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/common/RenderTarget.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/common/TexturedPanel.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/common/TexturedPanel.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/main.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/main.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/GraphicsPlayground-Mac/stdafx.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/GraphicsPlayground-Mac/stdafx.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/Tiny/tinystr.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/Tiny/tinystr.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/Tiny/tinyxml.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/Tiny/tinyxml.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/Tiny/tinyxmlerror.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/Tiny/tinyxmlerror.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/Tiny/tinyxmlparser.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/Tiny/tinyxmlparser.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_Buffer.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_Buffer.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_BufferManager.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_BufferManager.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_Common.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_Common.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_IndexBuffer.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_IndexBuffer.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_Material.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_Material.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_MaterialManager.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_MaterialManager.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_Program.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_Program.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_ProgramManager.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_ProgramManager.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_Texture.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_Texture.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_TextureManager.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_TextureManager.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_VertexBuffer.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_VertexBuffer.cpp.o"
+  "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/wolf/W_VertexDeclaration.cpp" "/Users/kli/Documents/Graphics Programming/GraphicsPlayground-Mac/cmake-build-debug/CMakeFiles/GraphicsPlayground_Mac.dir/wolf/W_VertexDeclaration.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../GraphicsPlayground-Mac"
+  "../GraphicsPlayground-Mac/CityModelExample"
+  "../GraphicsPlayground-Mac/Interpolation"
+  "../GraphicsPlayground-Mac/RotateCube"
+  "../GraphicsPlayground-Mac/RotateTextureCube"
+  "../GraphicsPlayground-Mac/BlendExample"
+  "../GraphicsPlayground-Mac/WaterRendering"
+  "../GraphicsPlayground-Mac/ParticleSystem"
+  "../GraphicsPlayground-Mac/common"
+  "../SimpleOpenGLImageLibrary/src"
+  "../Tiny"
+  "../wolf"
+  "../."
+  "/usr/local/include"
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
